b76b461c384eaba2b218e4e16c582bd333461365
hanzejian <hanzejian1998@163.com>
2023-02-12 03:20:36 +0800

    watch的用法

--- src/App.vue
+++ src/App.vue
@@ -3,69 +3,45 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>{{ count }}</h1>
     <h1>{{ double }}</h1>
-    <ul>
-      <li v-for="number in numbers" :key="number">
-        <h1>{{ number }}</h1>
-      </li>
-    </ul>
-    <h1>{{ person.name }}</h1>
+    <h1>{{ greetings }}</h1>
     <button @click="increase">+1</button>
+    <button @click="updateGreeting">Update Title</button>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, computed, reactive, toRefs, onMounted, onUpdated, onRenderTracked, watch } from 'vue';
+import { ref, computed, reactive, toRefs, watch } from 'vue';
 interface DataProps {
   count: number,
   increase: () => void
-  double: number,
-  numbers: number[],
-  person: { name?: string }
+  double: number
 }
 export default {
   name: 'App',
   // 准备阶段，在所有生命周期函数执行前执行，里面不能引用this
   setup() {
-    // const count = ref(0);
-    // console.log(count)
-    // const double = computed(() => {
-    //   return count.value * 2;
-    // })
-    // const increase = () => {
-    //   count.value++
-    // }
-    // return {
-    //   count,
-    //   increase,
-    //   double
-    // }
-    onMounted(() => {
-      console.log('mounted')
-    })
-    onUpdated(() => {
-      console.log('updated')
-    })
     // 起到调试作用，用以观测数据的变化
-    onRenderTracked((event) => {
-      console.log(event)
-    })
     const data: DataProps = reactive({
       count: 0,
       increase: () => { data.count++ },
-      double: computed(() => data.count * 2),
-      numbers: [0, 1, 2],
-      person: {}
+      double: computed(() => data.count * 2)
     })
-    data.numbers[0] = 5;
-    data.person.name = 'viking'
+    const greetings = ref('');
+    const updateGreeting = () => {
+      greetings.value += 'Hello! '
+    }
+    // watch可以同时监听多个值，若一个数据在响应对象里面，则需要用函数的方法监听
+    watch([greetings, () => data.count], (newVal, oldVal) => {
+      console.log(newVal);
+      console.log(oldVal);
+      document.title = 'update' + greetings.value + data.count
+    })
     const refData = toRefs(data)
     return {
-      ...refData
+      ...refData,
+      greetings,
+      updateGreeting
     }
-    // data是一个响应式对象，若是直接取出来则丧失响应性
-    // return {
-    //   ...data
-    // }
   }
 }
 </script>
